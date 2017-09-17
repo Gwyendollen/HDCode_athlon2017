a0552745768b4263d08ac7c17ea71b5623fb0567
Gwyendollen Mitchell <gwyendollen@gmail.com>
2017-09-16 20:15:31 -0400

    InitialCommit - from Collab
    From Unity Collab

---
+++ HomeDepotHackathon/Assets/DisplayTexture.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayTexture : MonoBehaviour {
+    public Texture2D camTexture;
+    public WebCamTexture webcamTex;
+    public Material displayImage;
+
+    //This boolean represents if a current feed is running or not 
+    bool running = true;
+
+	// Use this for initialization
+	void Start () {
+        initializeWebCam(640, 480);
+	}
+
+    //Initialize the webcam
+    public void initializeWebCam(int width, int height) {
+        print("There are currently " + WebCamTexture.devices.Length + " Webcams avilable on this computer");
+        webcamTex = new WebCamTexture();
+        webcamTex.requestedWidth = width;
+        webcamTex.requestedHeight = height;
+        webcamTex.Play();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        Color[] cols = webcamTex.GetPixels();
+        if (running) {
+            Texture2D newTex = new Texture2D(640, 480, TextureFormat.ARGB32, false);
+            newTex.SetPixels(cols);
+            newTex.Apply();
+            displayImage.mainTexture = newTex;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("PROCESSING WILL BEGIN");
+            running = !running;
+            Texture2D newTex = new Texture2D(640, 480, TextureFormat.ARGB32, false);
+            newTex.SetPixels(purge(webcamTex));
+            newTex.Apply();
+            displayImage.mainTexture = newTex;
+        }
+
+
+	}
+
+    //Will purge the background
+    public static Color[] purge(WebCamTexture tex) {
+        Color[] colArray = tex.GetPixels();
+        int i = 0;
+        while (i < colArray.Length) {
+            Color col = colArray[i];
+            float y = ((col.r * 255) * 0.21f) + ((col.g * 255) * 0.72f) + ((col.b * 255) * 0.07f);
+            if (y < 128) {
+                colArray[i] = Color.black;
+            } else {
+                colArray[i] = Color.white;
+            }
+            i += 1;
+        }
+        return colArray;
+    }
+
+
+}
---
+++ HomeDepotHackathon/Assets/ImageProcessing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageProcessing : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    //Will purge the background
+    public static Color[] purge(WebCamTexture tex) {
+        Color[] colArray = tex.GetPixels();
+        int i = 0;
+        while (i < colArray.Length) {
+            Color col = colArray[i];
+            float y = ((col.r * 255) * 0.21f) + ((col.g * 255) * 0.72f) + ((col.b * 255) * 0.07f);
+            if (y < 128) {
+                colArray[i] = Color.black;
+            } else {
+                colArray[i] = Color.white;
+            }
+            i += 1;
+        }
+        return colArray;
+    }
+}
